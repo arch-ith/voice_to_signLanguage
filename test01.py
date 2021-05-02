e8df5a7b9a2a1fda52557bf17aa114a2f5ac00e7
Dhanush V <54692379+dan-cymk17@users.noreply.github.com>
2021-05-02 18:45:55 +0530

    Video Stitch update

--- test01.py
+++ test01.py
@@ -2,7 +2,7 @@ from conf import SAMPLE_INPUTS,SAMPLE_OUTPUTS
 from moviepy.editor import *
 import glob, os
 
-s = "call Me"
+s = "call brother"
 s= list(s.split(" "))
 c=0
 dic = {}
@@ -16,11 +16,11 @@ for key, value in dic.items():
     for file in glob.glob("*.mp4"):
         if value.lower() ==os.path.splitext(file)[0].lower():
             clip = VideoFileClip(file)
-            if clip.size[1]>180:
-                clip=clip.resize(0.5)
+            #if clip.size[1]>180:
+                #clip=clip.resize(0.5)
             clips[key]=clip
             break
-clip = concatenate_videoclips(clips)
+clip = concatenate_videoclips(clips,method='compose')
 os.chdir(SAMPLE_OUTPUTS)
 clip.write_videofile("out1.mp4")
 
