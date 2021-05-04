8c0e2f537059562677f8c63cf4d51bf91b1e4284
arch-ith <archithkris@gmail.com>
2021-05-04 11:16:08 +0530

    added excpetion and talkinghands selenium

--- fetchLink.py
+++ fetchLink.py
@@ -2,6 +2,7 @@ from conf import DRIVER_DIR
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait  # for implicit and explict waits
 from selenium.webdriver.chrome.options import Options  # for suppressing the browser
 # !Make sure you use same VERSION OF CHROME AND DRIVER
@@ -11,12 +12,34 @@ def getLink(word):
     option.add_argument('headless')
     driver = webdriver.Chrome(PATH,options=option)
     #link to get video
-    time.sleep(1)
+    time.sleep(1)  
+    driver.get('https://www.talkinghands.co.in/video/'+word)
+    time.sleep(2) 
+    try:  
+        value_xpath=driver.find_element_by_xpath('/html/body/div[3]/div/div[2]/div[1]/div/div/div/div[13]/div[2]/div/div/div/iframe')
+        link=value_xpath.get_attribute('src')
+        print(link)
+        return link
+    except NoSuchElementException:
+        print("Element not found") 
+    
+    driver.get("https://www.talkinghands.co.in/video/"+word+"mp4")
+    try:
+        value_xpath=driver.find_element_by_xpath('/html/body/div[3]/div/div[2]/div/div[1]/div/div/div/video/source')
+        link=value_xpath.get_attribute('src') 
+        print(link)
+        return link
+    except NoSuchElementException:
+        print("Element not found") 
     driver.get("https://indiansignlanguage.org/search-dictionary/") 
-    driver.find_element_by_css_selector("a[href*="+word+"]").click() 
     time.sleep(1)
-    value_xpath=driver.find_element_by_xpath("/html/body/div/div[2]/div/div/div[1]/main/article/div/div/div/iframe")
-    link=value_xpath.get_attribute('src')
-    print(link) 
-    driver.quit()
-    return link  
+    driver.find_element_by_css_selector("a[href*="+word+"]").click() 
+    try:
+        value_xpath=driver.find_element_by_xpath("/html/body/div/div[2]/div/div/div[1]/main/article/div/div/div/iframe")
+        link=value_xpath.get_attribute('src')
+        print(link)
+        return link 
+    except NoSuchElementException:
+        print("Element not found")
+    driver.quit() 
+getLink("word")
