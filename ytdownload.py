29c0c54c637c1ad73685de8199143a08b047354b
Dhanush V <54692379+dan-cymk17@users.noreply.github.com>
2021-07-14 00:26:11 +0530

    Final changes

--- ytdownload.py
+++ ytdownload.py
@@ -1,6 +1,6 @@
 import fetchLink
 from pytube import YouTube
-from conf import SAMPLE_INPUTS,SAMPLE_OUTPUTS
+from conf import SAMPLE_INPUTS,SAMPLE_OUTPUTS,SAMPLES
 from moviepy.editor import *
 import os
 import glob
@@ -25,6 +25,7 @@ def downloader(word):
                 clips = [None]*len(w)
                 for i,l in enumerate(w):
                     filename= l+".mp4"
+                    os.chdir(SAMPLES)
                     filepath = os.path.join("alphabets", filename)
                     #print(filepath)
                     clip=  VideoFileClip(filepath)
